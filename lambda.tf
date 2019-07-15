8953295306465d53c7be536d7937939aa32fc53d
kondo takeshi <chaspy@users.noreply.github.com>
2019-07-15 23:21:28 +0900

    Add node app

--- lambda.tf
+++ lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_alias" "test_alias" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambdatest.zip"
+  filename      = "nodeapp.zip"
   function_name = "lambda_function_name"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.handler"
