b824e5f1956eb84f6de5495b5867c6c745c3f323
kondo takeshi <chaspy@users.noreply.github.com>
2019-07-22 04:00:02 +0900

    Update cloudwatch tf

--- cloud_watch.tf
+++ cloud_watch.tf
@@ -1,39 +1,41 @@
-resource "aws_cloudwatch_event_target" "yada" {
-  target_id = "Yada"
-  rule      = "${aws_cloudwatch_event_rule.console.name}"
-  arn       = "${aws_kinesis_stream.test_stream.arn}"
-
-  run_command_targets {
-    key    = "tag:Name"
-    values = ["FooBar"]
-  }
-
-  run_command_targets {
-    key    = "InstanceIds"
-    values = ["i-162058cd308bffec2"]
-  }
-}
-
 resource "aws_cloudwatch_event_rule" "console" {
-  name        = "capture-ec2-scaling-events"
-  description = "Capture all EC2 scaling events"
+  name        = "capture-aws-sign-in"
+  description = "Capture each AWS Console Sign In"
 
   event_pattern = <<PATTERN
 {
-  "source": [
-    "aws.autoscaling"
-  ],
   "detail-type": [
-    "EC2 Instance Launch Successful",
-    "EC2 Instance Terminate Successful",
-    "EC2 Instance Launch Unsuccessful",
-    "EC2 Instance Terminate Unsuccessful"
+    "AWS Console Sign In via CloudTrail"
   ]
 }
 PATTERN
 }
 
-resource "aws_kinesis_stream" "test_stream" {
-  name        = "terraform-kinesis-test"
-  shard_count = 1
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = "${aws_cloudwatch_event_rule.console.name}"
+  target_id = "SendToSNS"
+  arn       = "${aws_sns_topic.aws_logins.arn}"
+}
+
+resource "aws_sns_topic" "aws_logins" {
+  name = "aws-console-logins"
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = "${aws_sns_topic.aws_logins.arn}"
+  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = ["${aws_sns_topic.aws_logins.arn}"]
+  }
 }
